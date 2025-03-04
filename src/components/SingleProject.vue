e9d800879f958ab5b1a0b06f238c7fdc96bfe2b6
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2025-03-04 14:24:25 +0000

    Lesson 10 complete
    Add a EditProject view
    Populate the view with the necessary details.
    Set the view to allow props.

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,10 +3,10 @@
     <div class="actions">
         <h3 @click="handleClick"> {{ project.title }} </h3>
         <div class="icons">
-            
-<span class="material-icons">
+            <router-link :to="{ name: 'EditProject', params: {id: project.id} }"><span class="material-icons">
 edit
-</span>
+</span></router-link>
+
 <span @click="deleteProject" class="material-icons">
 delete
 </span>
